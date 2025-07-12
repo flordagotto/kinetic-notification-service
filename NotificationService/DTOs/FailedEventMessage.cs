5804f082cac6de72ec29fd74ea0f1e1202a8d082
florencia dagotto <flopydagotto@gmail.com>
2025-07-12 16:24:53 -0300

    add dlq

---
+++ NotificationService/DTOs/FailedEventMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace DTOs
+{
+    public class FailedEventMessage
+    {
+        public string OriginalMessage { get; set; } = default!;
+        public string? Error { get; set; }
+    }
+}
--- NotificationService/NotificationService/Services/RabbitMqConsumer.cs
+++ NotificationService/NotificationService/Services/RabbitMqConsumer.cs
@@ -18,6 +18,7 @@ namespace NotificationService.Services
         private readonly ILogger<RabbitMqConsumer> _logger;
         private readonly IConfiguration _configuration;
         private readonly IServiceProvider _serviceProvider;
+        private readonly int MaximumRetries;
 
         private readonly ConnectionFactory _factory;
         private readonly AsyncPolicyWrap _policyWrap;
@@ -28,7 +29,6 @@ namespace NotificationService.Services
         private const string PRODUCT_CREATED_QUEUE = "ProductCreated";
         private const string PRODUCT_UPDATED_QUEUE = "ProductUpdated";
         private const string PRODUCT_DELETED_QUEUE = "ProductDeleted";
-        private const string DEAD_LETTER_QUEUE_ROUTING_KEY = "error";
 
         private const string EXCHANGE_NAME = "inventory_exchange";
 
@@ -42,6 +42,8 @@ namespace NotificationService.Services
             _logger = logger;
             _configuration = configuration;
             _serviceProvider = serviceProvider;
+
+            MaximumRetries = int.Parse(_configuration["RabbitMQ:MaximumRetries"]);
 
             _factory = new ConnectionFactory()
             {
@@ -97,44 +99,68 @@ namespace NotificationService.Services
             consumer.ReceivedAsync += async (model, ea) =>
             {
                 var body = ea.Body.ToArray();
-                var messageString = Encoding.UTF8.GetString(body);
+                var props = ea.BasicProperties;
+                var routingKey = ea.RoutingKey;
+                int retries = 0;
 
-                EventMessage eventMessage = null;
+                if (props.Headers != null && props.Headers.ContainsKey("x-retries"))
+                {
+                    retries = Convert.ToInt32(props.Headers["x-retries"]);
+                }
+                var messageString = Encoding.UTF8.GetString(body);
 
                 try
                 {
-                    eventMessage = JsonSerializer.Deserialize<EventMessage>(messageString, _options);
+                    if (!string.IsNullOrEmpty(messageString))
+                        await ProcessMessage(messageString, stoppingToken);
+
+                    await _channel.BasicAckAsync(ea.DeliveryTag, false);
                 }
                 catch (Exception ex)
                 {
-                    var attempts = ea.BasicProperties.Headers?.ContainsKey("attempts") == true
-                     ? (int)ea.BasicProperties.Headers["attempts"] + 1
-                     : 1;
-
-                    if (attempts >= 3)
+                    if (retries < MaximumRetries)
                     {
-                        _logger.LogError("Message failed 3 times, sending to DLQ");
-                        await SendToErrorQueue(body); 
-                        await _channel.BasicNackAsync(ea.DeliveryTag, false, false); 
-                        return;
+                        _logger.LogInformation($"Retrying ({retries + 1}/{MaximumRetries}).");
+
+                        await _channel.BasicPublishAsync(
+                            EXCHANGE_NAME,
+                            routingKey,
+                            mandatory: true,
+                            basicProperties: new BasicProperties
+                            {
+                                Headers = new Dictionary<string, object?>
+                                {
+                                    {"x-retries", retries + 1 }
+                                }
+                            },
+                            body: body);
                     }
                     else
                     {
-                        ea.BasicProperties.Headers["attempts"] = attempts;
-                        await _channel.BasicNackAsync(ea.DeliveryTag, false, true); 
+                        _logger.LogInformation($"Maximum attemps reached. Sending to DLQ.");
+
+                        var failedMessage = new FailedEventMessage
+                        {
+                            OriginalMessage = messageString,
+                            Error = ex.Message.ToString()
+                        };
+
+                        var failedBody = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(failedMessage));
+
+                        await _channel.BasicPublishAsync(
+                            EXCHANGE_NAME,
+                            $"{routingKey}.dlq",
+                            mandatory: true,
+                            basicProperties: new BasicProperties
+                            {
+                                Headers = new Dictionary<string, object?>
+                                {
+                                    {"x-retries", retries }
+                                }
+                            },
+                            body: failedBody);
                     }
                 }
-
-                if (eventMessage != null)
-                {
-                    _logger.LogInformation($"Message received from queue: {ea.RoutingKey}");
-
-                    using var scope = _serviceProvider.CreateScope();
-                    var handler = scope.ServiceProvider.GetRequiredService<IInventoryMessageHandler>();
-                    await handler.HandleMessage(eventMessage, stoppingToken);
-                }
-
-                await _channel.BasicAckAsync(ea.DeliveryTag, false);
             };
 
             foreach (var queue in queues)
@@ -146,14 +172,16 @@ namespace NotificationService.Services
             await Task.Delay(Timeout.Infinite, stoppingToken);
         }
 
-        private async Task SendToErrorQueue(ReadOnlyMemory<byte> body)
+        private async Task ProcessMessage(string messageString, CancellationToken stoppingToken)
         {
-            await _channel.BasicPublishAsync(
-                exchange: EXCHANGE_NAME,
-                routingKey: DEAD_LETTER_QUEUE_ROUTING_KEY,
-                mandatory: true,
-                basicProperties: new BasicProperties { Persistent = true },
-                body: body);
+            var eventMessage = JsonSerializer.Deserialize<EventMessage>(messageString, _options);
+
+            if (eventMessage != null)
+            {
+                using var scope = _serviceProvider.CreateScope();
+                var handler = scope.ServiceProvider.GetRequiredService<IInventoryMessageHandler>();
+                await handler.HandleMessage(eventMessage, stoppingToken);
+            }
         }
 
         public override void Dispose()
